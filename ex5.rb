5036b72ebe43321054f876b480d08c92c57c7555
exgame1337 <adminuxas@gmail.com>
2017-08-01 14:06:29 +0300

    Exercise 5: More Variables and Printing

---
+++ ex5.rb
@@ -0,0 +1,16 @@
+name = 'Žygimantas'
+age = 24
+height = 185
+weight = 85
+eyes = 'Blue'
+teeth = 'White'
+hair = 'Brown'
+
+puts "Let's talk about #{name}"
+puts "He's #{height} cm tall."
+puts "He's #{weight} kg heavy."
+puts "Actually that's not too heavy."
+puts "He's got #{eyes} eyes and #{hair} hair."
+puts "His teeth are usualy #{teeth} depending on tea he consumes."
+
+puts "If I add #{age}, #{height}, and #{weight} I get #{age + height + weight}."
